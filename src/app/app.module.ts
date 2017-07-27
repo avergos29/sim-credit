782e11eb8192a4248112224be5fbf2a762e9f8f5
Antoine Vergos <antoine.vergos@gmail.com>
2017-07-27 23:46:15 +0200

    update dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
+
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
@@ -15,7 +17,7 @@ import { ButtonsModule } from 'ng2-bootstrap/buttons';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    ReactiveFormsModule,
     HttpModule,
     ButtonsModule.forRoot(),
   ],
--- src/app/credit-form/credit-form.component.ts
+++ src/app/credit-form/credit-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-credit-form',
@@ -6,22 +7,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./credit-form.component.css']
 })
 export class CreditFormComponent implements OnInit {
-  total : number = 600000;
-  apport : number = 250000;
-  taux : number = 1.90;
-  duree : number = 25;
-  result : number = 0;
+  private formGroup : FormGroup;
+  private hidden: boolean = true;
+  private result: number;
 
-  constructor() { }
+  constructor(private fb: FormBuilder){
+    this.formGroup = this.fb.group({
+        total : [600000, Validators.compose([Validators.pattern('^(0|[1-9][0-9]*)$' ), Validators.required])],
+        apport: [250000, Validators.required],
+        taux: [1.9, Validators.required],
+        duree: [25, Validators.required]
+    })
+  }
 
   ngOnInit() {
     this.compute();
   }
 
+  private value(name: string): number {
+    return this.formGroup.value[name];
+  }
+
+  private hasError(name: string) : string {
+     return this.formGroup.controls[name].invalid ? 'has-error' : '';
+  }
+
+  public validate(){
+    this.compute();
+    this.hidden = false;
+  }
+
   public compute(){
-    const txMens = (this.taux / 100 /12);
-    const duration = this.duree *12;
-    const amount = this.total - this.apport;
+    console.log('formValid', this.formGroup.valid);
+    const txMens = (this.value('taux') / 100 /12);
+    const duration = this.value('duree') *12;
+    const amount = this.value('total') - this.value('apport');
     this.result = (amount * txMens)/(1-Math.pow(1 + txMens,-duration));
     this.result = Math.round(this.result*100)/100;
     console.log('compute = ' + this.result);
